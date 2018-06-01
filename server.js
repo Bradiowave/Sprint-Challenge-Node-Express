43e6fecca5112ad9e6bba13f2cfa8cd1f5b6d461
Bradiowave <divebombwalker@gmail.com>
2018-06-01 14:08:32 -0700

    added get and getbyID methods

--- server.js
+++ server.js
@@ -43,10 +43,55 @@ server.post('/api/actions', (req, res) => {
 })
 
 
-//---------- Get Methods ----------
+//---------- Basic Get Methods ----------
+
+server.get('/api/projects', (req, res) => {
+    projects.get()
+        .then(projects => {
+            res.json({ projects })
+        })
+        .catch(err => {
+            res.json({ err })
+        })
+})
+
+server.get('/api/actions', (req, res) => {
+    actions.get()
+        .then(actions => {
+            res.json({ actions })
+        })
+        .catch(err => {
+            res.json({ err })
+        })
+})
+
+//---------- Get by ID Methods ----------
+
+server.get('/api/projects/:id', (req, res) => {
+    let { id } = req.params;
+    projects.get(id)
+        .then(project => {
+            res.json({ project })
+        })
+        .catch(err => {
+            res.status(400).json({ errorMessage: `No project found with id of ${id}` });
+        })
+})
+
+server.get('/api/actions/:id', (req, res) => {
+    let { id } = req.params;
+    actions.get(id)
+        .then(action => {
+            res.json({ action })
+        })
+        .catch(err => {
+            res.status(400).json({ errorMessage: `No action found with id of ${id}` });
+        })
+})
 
 
 //---------- Put Methods ----------
+
 
 
 //---------- Delete Methods ----------
